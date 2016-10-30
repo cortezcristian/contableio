42f6a8de9467dd44780127a873b4ae66fbe84ba4
Cristian Cortez <cortez.cristian@gmail.com>
2016-10-30 19:47:08 -0300

    Linking AngularJS

---
+++ src/app.js
@@ -0,0 +1,26 @@
+angular
+  .module('contableio', [
+    'toastr',
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ui.router',
+    'ngSanitize',
+    'toggle-switch',
+    'cfp.hotkeys',
+    'ui.grid',
+    'ui.grid.expandable',
+    'ui.grid.selection',
+    'ui.grid.pinning',
+    'ui.grid.pagination',
+    'ui.grid.edit',
+    'ui.grid.cellNav',
+    'ui.grid.exporter',
+    'ui.select',
+    'datetimepicker',
+    'ui.bootstrap'
+  ])
+  .config(function ($stateProvider, $locationProvider, uiSelectConfig, datetimepickerProvider, hotkeysProvider, toastrConfig) {
+		debugger;
+	});
+
