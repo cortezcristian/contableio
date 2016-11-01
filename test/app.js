86858628d34a16319b860d18551e2da40d95bca4
Cristian Cortez <cortez.cristian@gmail.com>
2016-11-01 08:05:29 -0300

    Adding wait to initialization

--- test/app.js
+++ test/app.js
@@ -15,33 +15,34 @@ describe('application launch', function () {
     console.log("Before each starts", process.cwd());
     var path_bar = process.cwd()+'/dist/Contableio-'+platform_name+'-x64/Contableio' + ( (platform_name === 'darwin') ? '.app/Contents/MacOS/Contableio' : '' );
     app = new Application({
-      path: path_bar
+      path: path_bar,
+      waitTimeout: 4000
     });
     console.log("About to return", path_bar);
     console.log("See __dirname", __dirname);
     app.start()
-			.then(function () {
-				// Check if the window is visible
-				return app.browserWindow.isVisible()
-			}).then(function (isVisible) {
-				// Verify the window is visible
-				console.log("Is visible?", isVisible)
-			}).then(function () {
-				// Get the window's title
-				return app.client.getTitle()
-			}).then(function (title) {
-				// Verify the window's title
-				console.log("Title:", title)
-			})
-			.then(function () {
-				console.log("Successfully started!", app.client.getTitle());
-				done();
-			})
-			.catch(function (error) {
-				// Log any failures
-				console.error('Test failed', error.message)
-				done(error);
-			});
+      .then(function () {
+        // Check if the window is visible
+        return app.browserWindow.isVisible()
+      }).then(function (isVisible) {
+        // Verify the window is visible
+        console.log("Is visible?", isVisible)
+      }).then(function () {
+        // Get the window's title
+        return app.client.getTitle()
+      }).then(function (title) {
+        // Verify the window's title
+        console.log("Title:", title)
+      })
+      .then(function () {
+        console.log("Successfully started!", app.client.getTitle());
+        done();
+      })
+      .catch(function (error) {
+        // Log any failures
+        console.error('Test failed', error.message)
+        done(error);
+      });
   })
 
   afterEach(function () {
