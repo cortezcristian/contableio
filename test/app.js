d7080a2330ed28e5aceb24b75ec02742ed4179c4
Cristian Cortez <cortez.cristian@gmail.com>
2016-11-01 00:44:00 -0300

    Wait more for linux

--- test/app.js
+++ test/app.js
@@ -8,7 +8,7 @@ if(platform_name.match(/linux|darwin/)) {
 console.log("Running on.."+ platform_name);
 
 describe('application launch', function () {
-  this.timeout(100000)
+  this.timeout(2*60*1000)
 
   beforeEach(function () {
     this.app = new Application({
