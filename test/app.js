9c523abf31791f85c000cf260f2a23ac04137d17
Cristian Cortez <cortez.cristian@gmail.com>
2016-11-01 00:34:08 -0300

    Give more time to linux

--- test/app.js
+++ test/app.js
@@ -8,7 +8,7 @@ if(platform_name.match(/linux|darwin/)) {
 console.log("Running on.."+ platform_name);
 
 describe('application launch', function () {
-  this.timeout(10000)
+  this.timeout(30000)
 
   beforeEach(function () {
     this.app = new Application({
