abae8c52edd0211ceeb35f58ba3c0eaf4b17e33d
Cristian Cortez <cortez.cristian@gmail.com>
2016-11-01 08:27:34 -0300

    Update screen settings

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-	/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-	echo "Starting xvfb..."
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+  sleep 3
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+  echo "Starting xvfb..."
 fi
 
 node --version
@@ -13,7 +16,7 @@ npm install
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]
 then
   electron-packager . --out=dist --asar --overwrite --platform=linux
-	ls -al dist/Contableio-linux-x64
+  ls -al dist/Contableio-linux-x64
 else
   electron-packager . --out=dist --asar --overwrite --platform=darwin
 fi
