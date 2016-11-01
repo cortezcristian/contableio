0834a620b9019a6ec7a38e5b49b5b571e46cc1be
Cristian Cortez <cortez.cristian@gmail.com>
2016-11-01 07:03:11 -0300

    Run xvfb as before script

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
 	echo "Starting xvfb..."
-  sleep 3
 fi
 
 node --version
