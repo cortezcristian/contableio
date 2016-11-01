0a139c919651aa7dc6283432f911bc62179dd276
Cristian Cortez <cortez.cristian@gmail.com>
2016-11-01 06:11:11 -0300

    Scan dir

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,6 +3,7 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export DISPLAY=:99.0
   sh -e /etc/init.d/xvfb start
+	echo "Starting xvfb..."
   sleep 3
 fi
 
@@ -14,7 +15,7 @@ npm install
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]
 then
   electron-packager . --out=dist --asar --overwrite --platform=linux
-	ls dist/Contableio-linux-x64
+	ls -al dist/Contableio-linux-x64
 else
   electron-packager . --out=dist --asar --overwrite --platform=darwin
 fi
